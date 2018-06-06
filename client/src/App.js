7148a6912cd23afc63d7cca134a68afd83b6ce7d
unknown <ancheco7@gmail.com>
2018-06-06 10:20:41 -0700

    created main CSS file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
+import './index.css';
 import './App.css';
+import Navibar from './navbar.js';
+import Footer from './footer.js';
 
 class App extends Component {
-  state = { test: [] }
-
-  componentDidMount() {
-    fetch('/test')
-    .then(res => res.json())
-    .then(test => this.setState({ test }))
-  }
-
   render() {
     return (
       <div className="App">
-        <h1 className="App-intro">
-          <ul>
-            {this.state.test.map(item =>
-              <li key={item.id}>{item.name}</li>
-            )}
-          </ul>
-        </h1>
+        <Navibar />
+      <div className="AppContent"> Some Stuff </div>
+      <Footer />
       </div>
     );
   }
---
+++ client/src/navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './index.css';
+import { Button, Navbar, Nav, NavItem, FormGroup, FormControl } from 'react-bootstrap';
+
+class Navibar extends Component {
+  render() {
+    return (
+      <div>
+       <Navbar>
+  <Navbar.Header className="navbarStyle">
+    <Navbar.Brand>
+      <a href="#home">MovieFive</a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#profile">
+      Profile
+    </NavItem>
+    <NavItem eventKey={2} href="#about">
+      About
+    </NavItem>
+    <NavItem eventKey={3} href="#login">
+      Login
+    </NavItem>
+  </Nav>
+  <Navbar.Collapse>
+    <Navbar.Form pullRight>
+      <FormGroup>
+        <FormControl type="text" placeholder="Search" />
+      </FormGroup>{' '}
+      <Button bsStyle="info" type="submit">Search</Button>
+    </Navbar.Form>
+  </Navbar.Collapse>
+</Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navibar;
