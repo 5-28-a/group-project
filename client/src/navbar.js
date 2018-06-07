fa3f7ec8ae0903bee5c42f6e40fe02cbc1fd8b68
unknown <ancheco7@gmail.com>
2018-06-06 22:14:17 -0700

    added MovieFive logo, slight tweaks to CSS

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -7,22 +7,22 @@ class Navibar extends Component {
     return (
       <div>
        <Navbar>
-  <Navbar.Header className="navbarStyle">
-    <Navbar.Brand>
-      <a href="#home">MovieFive</a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav>
-    <NavItem eventKey={1} href="#profile">
-      Profile
-    </NavItem>
-    <NavItem eventKey={2} href="#about">
-      About
-    </NavItem>
-    <NavItem eventKey={3} href="#login">
-      Login
-    </NavItem>
-  </Nav>
+         <Navbar.Header className="navbarStyle">
+          <Navbar.Brand>
+            <a href="#home"><img id="brand-image" src="mvlogopng.png" alt="MVistheMVP" height={40}/>MovieFive</a>
+          </Navbar.Brand>
+         </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#profile">
+            Profile
+          </NavItem>
+          <NavItem eventKey={2} href="#about">
+            About
+          </NavItem>
+          <NavItem eventKey={3} href="#login">
+            Login
+          </NavItem>
+        </Nav>
   <Navbar.Collapse>
     <Navbar.Form pullRight>
       <FormGroup>
